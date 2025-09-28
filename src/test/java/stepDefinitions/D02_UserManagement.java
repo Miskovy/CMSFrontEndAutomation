c9b82aa735891c87bff2a0270a27ca09660d23ea
Miskovy <mazenkhairy200@gmail.com>
2025-09-28 11:05:45 +0300

    Update D02_UserManagement.java
    commit 2

--- src/test/java/stepDefinitions/D02_UserManagement.java
+++ src/test/java/stepDefinitions/D02_UserManagement.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.messages.types.Hook;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
